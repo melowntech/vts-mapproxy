6da9cfc2f150fcd4dad25fa11af320eccc87ce1d
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-07-01 14:31:01 +0200

    removed accidentally commited crap

--- mapproxy/refine.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./bin/vts tmp/output/rpp2 --readd jenstejn; ./bin/vts tmp/output/rpp2/glues/jenstejn2015_jenstejn --tile-pick --tileset tmp/output/j.pick --overwrite --tileId 17-65191-64576
