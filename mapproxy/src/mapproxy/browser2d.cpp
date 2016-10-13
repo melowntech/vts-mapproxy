5fd2a55cc54e342e0d1db2f7fdaa7c7fa50b8360
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-10-13 11:56:57 +0200

    latest table leaflet hosted on HTTPS site -> using external leaflet

--- mapproxy/src/mapproxy/browser2d.cpp
+++ mapproxy/src/mapproxy/browser2d.cpp
@@ -1,8 +1,6 @@
 #include "./browser2d.hpp"
 
 #include "browser2d/index.html.hpp"
-#include "browser2d/leaflet.js.hpp"
-#include "browser2d/leaflet.css.hpp"
 
 namespace browser2d {
 
@@ -14,20 +12,6 @@ const vadstena::storage::SupportFile::Files supportFiles =
             , sizeof(index_html)
             , index_html_attr_lastModified
             , "text/html; charset=utf-8"
-        }
-    }, { "leaflet.js"
-         , {
-            leaflet_js
-            , sizeof(leaflet_js)
-            , leaflet_js_attr_lastModified
-            , "application/javascript;  charset=utf-8"
-        }
-    }, { "leaflet.css"
-         , {
-            leaflet_css
-            , sizeof(leaflet_css)
-            , leaflet_css_attr_lastModified
-            , "text/css; charset=utf-8"
         }
     }
 };
