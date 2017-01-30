93bb998c4756d9a69e9f73bfd04bb5b6fc63780a
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-01-30 18:16:15 +0100

    surface: added nominalTexelSize and mergeBottomLod; tms: added transparent flag (forces png format, writting to boundlayer.json)

--- mapproxy/src/mapproxy/generator/surface.cpp
+++ mapproxy/src/mapproxy/generator/surface.cpp
@@ -9,6 +9,9 @@
 #include "imgproc/rastermask/cvmat.hpp"
 #include "imgproc/png.hpp"
 
+#include "jsoncpp/json.hpp"
+#include "jsoncpp/as.hpp"
+
 #include "vts-libs/storage/fstreams.hpp"
 #include "vts-libs/vts/io.hpp"
 #include "vts-libs/vts/nodeinfo.hpp"
@@ -22,12 +25,15 @@
 #include "vts-libs/vts/2d.hpp"
 #include "vts-libs/vts/debug.hpp"
 #include "vts-libs/vts/mapconfig.hpp"
+#include "vts-libs/registry/json.hpp"
+#include "vts-libs/registry/py.hpp"
 
 #include "../error.hpp"
 #include "../support/metatile.hpp"
 #include "../support/mesh.hpp"
 #include "../support/srs.hpp"
 #include "../support/grid.hpp"
+#include "../support/python.hpp"
 
 #include "./surface.hpp"
 
@@ -37,6 +43,111 @@ namespace vs = vadstena::storage;
 namespace vts = vadstena::vts;
 
 namespace generator {
+
+void SurfaceBase::SurfaceDefinition::parse(const Json::Value &value)
+{
+    if (value.isMember("nominalTexelSize")) {
+        nominalTexelSize = boost::in_place();
+        Json::get(*nominalTexelSize, value, "nominalTexelSize");
+    }
+
+    if (value.isMember("mergeBottomLod")) {
+        mergeBottomLod = boost::in_place();
+        Json::get(*mergeBottomLod, value, "mergeBottomLod");
+    }
+
+    if (value.isMember("introspection")) {
+        const auto &introspection(value["introspection"]);
+        if (introspection.isMember("tms")) {
+            const auto &surface(introspection["tms"]);
+            Resource::Id rid;
+            Json::get(rid.group, surface, "group");
+            Json::get(rid.id, surface, "id");
+            introspectionTms = rid;
+        }
+
+        if (introspection.isMember("position")) {
+            introspectionPosition
+                = vr::positionFromJson(introspection["position"]);
+        }
+    }
+}
+
+void SurfaceBase::SurfaceDefinition::build(Json::Value &value) const
+{
+    if (nominalTexelSize) {
+        value["nominalTexelSize"] = *nominalTexelSize;
+    }
+    if (mergeBottomLod) {
+        value["mergeBottomLod"] = *mergeBottomLod;
+    }
+
+    if (introspectionTms || introspectionPosition) {
+        auto &introspection(value["introspection"] = Json::objectValue);
+        if (introspectionTms) {
+            auto &tms(introspection["tms"] = Json::objectValue);
+            tms["group"] = introspectionTms->group;
+            tms["id"] = introspectionTms->id;
+        }
+        if (introspectionPosition) {
+            introspection["position"] = vr::asJson(*introspectionPosition);
+        }
+    }
+}
+
+void SurfaceBase::SurfaceDefinition::parse(const boost::python::dict &value)
+{
+    if (value.has_key("nominalTexelSize")) {
+        nominalTexelSize
+            = boost::python::extract<double>(value["nominalTexelSize"]);
+    }
+
+    if (value.has_key("mergeBottomLod")) {
+        mergeBottomLod = boost::python::extract
+            <vts::Lod>(value["mergeBottomLod"]);
+    }
+
+    if (value.has_key("introspection")) {
+        boost::python::dict introspection(value["introspection"]);
+        if (introspection.has_key("tms")) {
+            boost::python::dict tms(introspection["tms"]);
+            Resource::Id rid;
+            rid.group = py2utf8(tms["group"]);
+            rid.id = py2utf8(tms["id"]);
+            introspectionTms = rid;
+        }
+
+        if (introspection.has_key("position")) {
+            introspectionPosition = boost::in_place();
+            vr::fromPython(*introspectionPosition
+                           , introspection["position"]);
+        }
+    }
+}
+
+Changed SurfaceBase::SurfaceDefinition::changed_impl(const DefinitionBase &o)
+    const
+{
+    const auto &other(o.as<SurfaceDefinition>());
+
+    // manually set data can be changed safely
+    if (nominalTexelSize != other.nominalTexelSize) {
+        return Changed::safely;
+    }
+    if (mergeBottomLod != other.mergeBottomLod) {
+        return Changed::safely;
+    }
+
+    // introspection can safely change
+    if (introspectionTms != other.introspectionTms) {
+        return Changed::safely;
+    }
+    if (introspectionPosition != other.introspectionPosition) {
+        return Changed::safely;
+    }
+
+    return Changed::no;
+}
 
 fs::path SurfaceBase::filePath(vts::File fileType) const
 {
@@ -55,6 +166,54 @@ SurfaceBase::SurfaceBase(const Params &params)
     : Generator(params)
     , index_(resource().referenceFrame->metaBinaryOrder)
 {}
+
+bool SurfaceBase::loadFiles(const SurfaceDefinition &definition)
+{
+    try {
+        auto indexPath(filePath(vts::File::tileIndex));
+        auto propertiesPath(filePath(vts::File::config));
+        if (fs::exists(indexPath) && fs::exists(propertiesPath)) {
+            // both paths exist -> ok
+            vts::tileset::loadTileSetIndex(index_, indexPath);
+            properties_ = vts::tileset::loadConfig(propertiesPath);
+            if (updateProperties(definition)) {
+                // something changed in properties, update
+                vts::tileset::saveConfig(filePath(vts::File::config)
+                                         , properties_);
+            }
+
+            makeReady();
+            return true;
+        }
+    } catch (const std::exception &e) {
+        // not ready
+    }
+
+    LOG(info1) << "Generator for <" << id() << "> not ready.";
+    return false;
+}
+
+bool SurfaceBase::updateProperties(const SurfaceDefinition &def)
+{
+    bool changed(false);
+
+    if (properties_.nominalTexelSize != def.nominalTexelSize) {
+        properties_.nominalTexelSize = def.nominalTexelSize;
+        changed = true;
+    }
+
+    if (def.mergeBottomLod) {
+        if (properties_.mergeBottomLod != *def.mergeBottomLod) {
+            properties_.mergeBottomLod = *def.mergeBottomLod;
+            changed = true;
+        }
+    } else if (properties_.mergeBottomLod) {
+        properties_.mergeBottomLod = 0;
+        changed = true;
+    }
+
+    return changed;
+}
 
 Generator::Task SurfaceBase
 ::generateFile_impl(const FileInfo &fileInfo, Sink &sink) const
