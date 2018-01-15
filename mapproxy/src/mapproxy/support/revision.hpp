3f8f7c4fdd060fec20dd05544710237459558c80
Vaclav Blazek <vaclav.blazek@citationtech.net>
2018-01-15 13:33:34 +0100

    added RevisionWrapper for revision-to-stream writer

---
+++ mapproxy/src/mapproxy/support/revision.hpp
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2017 Melown Technologies SE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * *  Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef mapproxy_support_revision_hpp_included_
+#define mapproxy_support_revision_hpp_included_
+
+#include <iostream>
+
+struct RevisionWrapper {
+    unsigned int revision;
+    const char *sep;
+
+    RevisionWrapper(unsigned int revision = 0, const char *sep = "")
+        : revision(revision), sep(sep)
+    {}
+};
+
+template<typename CharT, typename Traits>
+inline std::basic_ostream<CharT, Traits>&
+operator<<(std::basic_ostream<CharT, Traits> &os, const RevisionWrapper &r)
+{
+    if (!r.revision) { return os; }
+    return os << r.sep << "r=" << r.revision;
+}
+
+#endif // mapproxy_support_revision_hpp_included_
